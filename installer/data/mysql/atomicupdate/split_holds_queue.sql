fa3ffba65ffa1a643c745a770ab71a0fbf27d1e2
Kyle M Hall <kyle@bywatersolutions.com>
2017-09-30 08:02:57 -0400

    Bug XXX - Add syspref

---
+++ installer/data/mysql/atomicupdate/split_holds_queue.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+('HoldsSplitQueue','nothing','nothing|branch|itemtype|branch_itemtype','In the staff client, split the holds view by the given criteria','Choice');
+
